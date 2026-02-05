3158fbbaaf7bc809fd50da8c6bd5eb4f9e137882
Thiago Vidal <thiagomvnn@gmail.com>
2026-02-05 20:35:32 -0300

    Lead and contact structure created📞✅

---
+++ src/thiagocrm/crm/leads/Contact.java
@@ -0,0 +1,8 @@
+package thiagocrm.crm.leads;
+
+public class Contact {
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String email;
+}
---
+++ src/thiagocrm/crm/leads/Lead.java
@@ -0,0 +1,7 @@
+package thiagocrm.crm.leads;
+
+public class Lead{
+    private Contact contact;
+    private int leadId;
+    private String leadName;
+}
